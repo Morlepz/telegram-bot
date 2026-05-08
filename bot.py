a3564ebfd7c334dbe9b9aa6f470e99ff32d0fd7e
Morlepz <mezahirkishi12@gmail.com>
2026-05-08 21:23:15 +0400

    Update bad_words list in bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ sys.stdout.reconfigure(encoding='utf-8')
 TOKEN = os.getenv("TOKEN")
 
 bad_words = ["sik", "sikim", "peyser", "peysər", "cindir", "cındır", "pesi", "qehbe",
-             "qəhbə", "orospu", "oç", "gijdıllaq", "gijdillax", "gidjillax", "gijdılax", "gijdilax", "siktir", "sikdir", "osdur", "balası", "skdr", "götverən", "qancix", "yarrak", "yarak", "taşak", "am", "amina koyim", "ananı", "skm", "anneni", "amına"]
+             "qəhbə", "orospu", "oç", "göt", "got", "gijdıllaq", "gijdillax", "gidjillax", "gijdılax", "gijdilax", "siktir", "sikdir", "osdur", "balası", "skdr", "götverən", "qancix", "yarrak", "yarak", "taşak", "am", "amina koyim", "ananı", "skm", "anneni", "amına"]
 
 warns = {}
 
